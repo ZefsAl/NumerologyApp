a870c662b73a097975d4338399f15240f042cc60
ZefsAl <sergeimem@gmail.com>
2025-07-24 15:30:17 +0400

    v_1.0.37_dev

---
+++ Numerology/Manager/TokensManager.swift
@@ -0,0 +1,15 @@
+//
+//  TokensManager.swift
+//  Numerology
+//
+//  Created by Serj_M1Pro on 24.07.2025.
+//
+
+import Foundation
+
+
+class TokensManager {
+    static let shared = TokensManager()
+    static let revenuecat = ""
+    static let deepseek = ""
+}
